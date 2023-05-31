1026c63221033c8fc6672e52c76b400199e2ca6f
nassims01 <nassimsalomon01@gmail.com>
2023-05-31 17:08:30 -0300

    avance detallado de juegos

--- js/games.js
+++ js/games.js
@@ -1,53 +1,78 @@
 // rol administrador
 document.addEventListener("DOMContentLoaded", function () {
-    // Verificar el rol de administrador
-    let rol = localStorage.getItem("rol");
-    if (rol === "administrador") {
-        // Mostrar el botón de cerrar sesión
-        let cerrarSesion = document.getElementById("cerrarSesion");
-        cerrarSesion.innerHTML = 'cerrar sesion |';
-        let etiqueta = document.getElementById("etiqueta")
-        etiqueta.innerHTML = 'Administrador';
+  // Verificar el rol de administrador
+  let rol = localStorage.getItem("rol");
+  if (rol === "administrador") {
+    // Mostrar el botón de cerrar sesión
+    let cerrarSesion = document.getElementById("cerrarSesion");
+    cerrarSesion.innerHTML = "cerrar sesion |";
+    let etiqueta = document.getElementById("etiqueta");
+    etiqueta.innerHTML = "Administrador";
 
-        // ocultamos los botones de ingresar y registrar
-        let btnIngresar = document.getElementById("btn-ingresar"); 
-        let btnRegistro = document.getElementById("btn-registro"); 
-        btnIngresar.style.display = "none";
-        btnRegistro.style.display = "none";
+    // ocultamos los botones de ingresar y registrar
+    let btnIngresar = document.getElementById("btn-ingresar");
+    let btnRegistro = document.getElementById("btn-registro");
+    btnIngresar.style.display = "none";
+    btnRegistro.style.display = "none";
 
-
-        // Agregar el evento de clic al botón de cerrar sesión
-        cerrarSesion.addEventListener("click", function () {
-            // Eliminar el rol almacenado en localStorage
-            localStorage.removeItem("rol");
-            // Redirigir al inicio de sesión
-            window.location.href = "games.html";
-        });
-    }
+    // Agregar el evento de clic al botón de cerrar sesión
+    cerrarSesion.addEventListener("click", function () {
+      // Eliminar el rol almacenado en localStorage
+      localStorage.removeItem("rol");
+      // Redirigir al inicio de sesión
+      window.location.href = "games.html";
+    });
+  }
 });
 
 // rol usuario
 document.addEventListener("DOMContentLoaded", function () {
-    // Verificar el rol
-    let rol = localStorage.getItem("rol");
-    if (rol === "usuario") {
-        // Mostrar el botón de cerrar sesión
-        let cerrarSesion = document.getElementById("cerrarSesion");
-        cerrarSesion.innerHTML = 'cerrar sesion';
+  // Verificar el rol
+  let rol = localStorage.getItem("rol");
+  if (rol === "usuario") {
+    // Mostrar el botón de cerrar sesión
+    let cerrarSesion = document.getElementById("cerrarSesion");
+    cerrarSesion.innerHTML = "cerrar sesion";
 
-        // ocultamos los botones de ingresar y registrar
-        let btnIngresar = document.getElementById("btn-ingresar"); 
-        let btnRegistro = document.getElementById("btn-registro"); 
-        btnIngresar.style.display = "none";
-        btnRegistro.style.display = "none";
+    // ocultamos los botones de ingresar y registrar
+    let btnIngresar = document.getElementById("btn-ingresar");
+    let btnRegistro = document.getElementById("btn-registro");
+    btnIngresar.style.display = "none";
+    btnRegistro.style.display = "none";
 
-
-        // Agregar el evento de clic al botón de cerrar sesión
-        cerrarSesion.addEventListener("click", function () {
-            // Eliminar el rol almacenado en localStorage
-            localStorage.removeItem("rol");
-            // Redirigir al inicio de sesión
-            window.location.href = "games.html";
-        });
-    }
+    // Agregar el evento de clic al botón de cerrar sesión
+    cerrarSesion.addEventListener("click", function () {
+      // Eliminar el rol almacenado en localStorage
+      localStorage.removeItem("rol");
+      // Redirigir al inicio de sesión
+      window.location.href = "games.html";
+    });
+  }
 });
+
+const sliderDerecha = document.querySelector(".slider-derecho");
+const sliderIzquierdo = document.querySelector(".slider-izquierdo");
+
+document.Imagen.src = imagenes[0];
+
+let contador = 0;
+
+function moverDerecha() {
+  contador++;
+  if (contador > imagenes.length - 1) {
+    contador = 0;
+  }
+
+  document.Imagen.src = imagenes[contador];
+}
+
+function moverIzquierda() {
+  contador--;
+  if (contador < 0) {
+    contador = imagenes.length - 1;
+  }
+  document.Imagen.src = imagenes[contador];
+}
+
+sliderDerecha.addEventListener("click", moverDerecha);
+sliderIzquierdo.addEventListener("click", moverIzquierda);
