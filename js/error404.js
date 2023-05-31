c10f59663806ab5c619bc0bf00cd98d762bc8e48
Francisco <JoseFranciscoPaez1999@gmail.com>
2023-05-31 08:01:40 -0300

    agrego página de error 404 lista para usar

---
+++ js/error404.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+    let urlActual = window.location.href;
+  
+    if (urlActual.includes("error404.html")) {
+      window.location.href = "../index.html";
+    }
+  }, 5000);
+  
